252ec33e5c49d7a94e14fa70841ae6cd2d70fdad
Sirine Rajhi <sirine.rajhi@outlook.com>
2022-12-14 13:25:19 +0100

    added comments to payment

--- src/payment.cpp
+++ src/payment.cpp
@@ -1,38 +1,33 @@
 #include "payment.h"
 
-Payment::Payment() : Payment(nullptr, nullptr, 0) {}
+Payment::Payment() : Payment(nullptr, nullptr, 0) {}        // Default constructor van de Payment klasse. Deze roept de andere constructor aan een geeft mee dat er geen partijen en geen bedrag is.
 
-Payment::Payment(User * buyer, User * seller, int amount){
-    _buyer = buyer;
-    _seller = seller;
+Payment::Payment(User * buyer, User * seller, int amount){  // Constructor van de Payment klasse.
+    _buyer = buyer;                                         // Deze maakt per transactie (= per payment dus) een koper, verkoper en bedrag aan.
+    _seller = seller;                                       // De transactie kan dan door andere klasses uitgelezen worden en er de nodige partijen/gegevens uit halen.
     _amount = amount;
 }
-bool Payment::Execute(void){
-    this->_buyer->updateBalance(-(this->_amount));
-    this->_seller->updateBalance((this->_amount));
-    return true;
-}
 
-User * Payment::getSeller(void){
+User * Payment::getSeller(void){    // Functie om de verkoper uit de transactie uit te lezen.
     return this->_seller;
 }
 
-User * Payment::getBuyer(void){
+User * Payment::getBuyer(void){     // Functie om de koper uit de transactie uit te lezen.
     return this->_buyer;
 }
 
-int Payment::getAmount(void){
-    return this->_amount;
+int Payment::getAmount(void){       // Functie om het bedrag uit de transactie uit te lezen.                         
+    return this->_amount;   
 }
 
-void Payment::setAmount(int amount){
+void Payment::setAmount(int amount){    // Functie om het bedrag in de transactie aan te passen indien nodig.
     this->_amount = amount;
 }
 
-void Payment::setBuyer(User * buyer){
+void Payment::setBuyer(User * buyer){   // Functie om de koper in de transactie aan te passen indien nodig.
     this->_buyer = buyer;
 }
 
-void Payment::setSeller(User * seller){
+void Payment::setSeller(User * seller){     // Functie om de koper in de transactie aan te passen indien nodig.
     this->_seller = seller;
 }
--- src/payment.h
+++ src/payment.h
@@ -3,10 +3,10 @@
 
 class Payment {
 
+    // Functies van de Payment klasse (uitleg in .cpp file)
     public:
         Payment();
         Payment(User * buyer, User * seller, int amount);
-        bool Execute(void);
         User * getSeller(void);
         User * getBuyer(void);
         int getAmount(void);
@@ -14,9 +14,10 @@ class Payment {
         void setBuyer(User * buyer);
         void setSeller(User * seller);
 
+    // Attributen van de Payment klasse
     private:
-        User * _buyer = nullptr;
-        User * _seller = nullptr;
-        int _amount = 0;
+        User * _buyer = nullptr;    // De koper in de transactie
+        User * _seller = nullptr;   // De verkoper in de transactie
+        int _amount = 0;            // Het bedrag van de transactie
 
 };
